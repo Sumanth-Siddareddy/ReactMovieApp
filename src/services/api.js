c76cfdb4316fe7b2d7b7f566c1e5e11039a88a88
Sumanth-Siddareddy <siddareddy.venkatasumanth28@gmail.com>
2025-07-07 11:33:12 +0530

    My first react project, to learn useContext, useState, useEffect, React Routes, API calling, LighHouse

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "ab0a79b4bae5758748925d8ec1fa89c7"
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
